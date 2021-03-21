e445f8d51e6313fe993b9b64d14016e1be94bf91
Stetson Ramey <stetson.ramey@gmail.com>
2021-03-21 13:56:18 -0700

    module 2

---
+++ module-2-project/src/components/PerspectivePane/PerspecitvePane.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./PerspectivePane.module.css";
+
+const PerspectivePane = () => {
+  return <div className={`${styles.darkPane}`}></div>;
+};
+
+export default PerspectivePane;
